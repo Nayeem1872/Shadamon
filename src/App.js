783037e933571ad9414117a1f853a1980e4ff05c
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-02-03 22:12:27 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
-import React from "react";
-import Navbar from "./components/navbar/Navbar";
-import Sidebar from "./components/sidebar/Sidebar";
-import Demo from "./components/demo/Demo";
-import Login from "./components/login/Login";
-import Login1 from "./components/login/Login1";
-import Signup from "./components/login/Signup";
-import Home from "./components/main/home/home"
-import Search from "./components/search/Search";
-import AddStoryPost from "./components/storyAddPost/StoryAddPost"
-import Advertisement from "./components/advertisement/Advertisement";
+import Home from "./adminPages/home/Home";
+// import Login from "./adminPages/login/Login";
+// import List from "./adminPages/list/List";
+// import Single from "./adminPages/single/Single";
+// import New from "./adminPages/new/New"
+import Admin from "./Admin"
+import Categories from "./adminComponents/settings/categories/Categories";
+import Location from "./adminComponents/settings/location/Location";
+import Datatable from "./adminComponents/locationDatatable/LocationDatatable"
 
+
+import {
+  BrowserRouter,Route,Routes,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App ">
-      < Navbar />
-      <div className="flex bg-gray-100">
-      <div className="flex gap-[50px] absoloute inset-y-0 left-0 ml-[400px] bg">
-          <div className=" w-[250px] hidden lg:block">
-
-            < Sidebar />
-          
-          </div>
-           
-          < Login />
-          < Login1 />
-          <Signup />
-      </div>
-      <div className="grid gap-8 grid-cols-2 px-4">
-      < Home />
-      <div class="relative h-32 w-32 pr-4 bg-gray-100">
-        <div class="absolute inset-y-0 left-12 w-32 bg-gray-100 ">
-      < Advertisement />
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className=" flex justify-center items-center bg-gray-100">
-        <div className=" col-span-2">
-          < Search />
-          < AddStoryPost />
-          </div>
-        
-      </div>
+    <div className="App">
       
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/">
+            <Route index element={<Admin/>} />
+          <Route path="/admin" element={<Home/>}/>
+             <Route path="/admin/categories" element={< Categories />}/>
+             <Route path="/admin/location" element={< Location />}/>
+              
+              {/* <Route path="/admin/new" element={<New/>}/>
+              <Route path="/admin/categories" >
+                <Route index element={<Categories/>} />
+              </Route> 
+
+            <Route path="/admin/product" >
+            <Route index element={<List/>} />
+             <Route path="/admin/:productId" element={<Single/>}/>
+              
+             <Route path="/admin/new" element={<New/>}/> */}
+              {/* </Route>      */}
+            
+            
+            
+            
+            
+            </Route>
+          </Routes>
+
+
+
+
+      </BrowserRouter>
     </div>
   );
 }
