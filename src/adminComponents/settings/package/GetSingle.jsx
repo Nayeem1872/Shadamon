26251dbffc9777d50ebe8cf550c700d63bf08a9e
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-02-17 00:20:29 +0600

    updated

--- src/adminComponents/settings/package/GetSingle.jsx
+++ src/adminComponents/settings/package/GetSingle.jsx
@@ -1,8 +1,99 @@
 import React from 'react'
 
 const GetSingle = () => {
+ 
+ 
+ 
+ 
+ 
+ 
   return (
-    <div>GetSingle</div>
+    <>
+   <div>
+
+  
+      <div class="overflow-hidden">
+        <table class="min-w-full border text-center">
+          <thead class="border-b">
+            <tr>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                Sub-Categories
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                Reach(Promote)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                Reach(Traffic)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+              Click(Promote)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                Click(Traffic)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+              Price(Promote)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+              Price(Traffic)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+              MinAmount(Promote)
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+              MinAmount(Traffic)
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+         
+            <tr class="bg-white border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">2</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                Jacob
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                Jacob
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                Jacob
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                Jacob
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                Jacob
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                Thornton
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                @fat
+              </td>
+            
+            </tr>
+        
+          </tbody>
+        </table>
+      </div>
+    </div>
+  
+
+   
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    </>
   )
 }
 
